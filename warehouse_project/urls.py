2c45cb401d235d06ec6331b46452be8688931fe8
pixsis13 <142459193+pixsis13@users.noreply.github.com>
2025-12-01 23:49:10 +0330

    Update urls.py

--- warehouse_project/urls.py
+++ warehouse_project/urls.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
-from django.urls import path, include  # ۱. اضافه کردن include
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from inventory import views as inventory_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # مسیرهای لاگین و خروج پیش‌فرض جنگو
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    
+    # مسیر ثبت نام
+    path('register/', inventory_views.register, name='register'),
 
-    # ۲. هدایت صفحه اصلی به اپلیکیشن انبارداری
     path('', include('inventory.urls')),
 ]
